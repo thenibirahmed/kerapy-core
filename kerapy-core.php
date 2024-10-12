2423951cd45c4c3cda08cd21013e0b7b3c48949c
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-10-13 01:10:00 +0600

    init kerapy core

---
+++ kerapy-core.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * Plugin Name: Kerapy Core
+ * Plugin URI: https://kerapy.com
+ * Description: This is a core plugin for Kerapy theme.
+ * Version: 1.0
+ * Author: Kerapy
+ * Author URI: https://kerapy.com
+ * License: GPL2
+ * Text Domain: kerapy-core
+ * Domain Path: /languages
+ */
