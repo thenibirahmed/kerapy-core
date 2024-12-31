7cdb9300d04607d25d3b7b6b0a53d3a6c1f7f655
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-12-31 08:54:27 +0600

    fixed redux

--- lib/redux-framework/redux-core/inc/extensions/icon_select/class-redux-extension-icon-select.php
+++ lib/redux-framework/redux-core/inc/extensions/icon_select/class-redux-extension-icon-select.php
@@ -6,6 +6,8 @@
  * @author  Dovy Paukstys <dovy@reduxframework.com> & Kevin Provance <kevin.provance@gmail.com>
  * @class   Redux_Extension_Icon_Select
  * @version 4.4.2
+ *
+ * @noinspection PhpIgnoredClassAliasDeclaration
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -26,14 +28,14 @@ if ( ! class_exists( 'Redux_Extension_Icon_Select' ) ) {
 		 *
 		 * @var string
 		 */
-		public static $version = '4.4.2';
+		public static string $version = '4.4.2';
 
 		/**
 		 * Extension friendly name.
 		 *
 		 * @var string
 		 */
-		public $ext_name = 'Icon Select';
+		public string $extension_name = 'Icon Select';
 
 		/**
 		 * ReduxFramework_Extension_Icon_Select constructor.
@@ -123,6 +125,6 @@ if ( ! class_exists( 'Redux_Extension_Icon_Select' ) ) {
 			wp_send_json_success( array( 'content' => $content ) );
 		}
 	}
-}
 
-class_alias( 'Redux_Extension_Icon_Select', 'ReduxFramework_extension_icon_select' );
+	class_alias( Redux_Extension_Icon_Select::class, 'ReduxFramework_extension_icon_select' );
+}
